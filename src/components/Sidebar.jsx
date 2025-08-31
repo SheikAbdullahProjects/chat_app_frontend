cc314cf98b185933b076bbb15b704d20e4caf000
Sheik Abdullah <sheikabdullah.web@gmail.com>
2025-09-01 01:01:13 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -53,8 +53,8 @@ const Sidebar = () => {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "/avatar.png"}
-                alt={user.name}
+                src={user.profile_picture || "/avatar.png"}
+                alt={user.username}
                 className="size-12 object-cover rounded-full"
               />
               {onlineUsers.includes(String(user.id)) && (
