d0c7e2b95de951405a79af31e6ad36143445859c
Sheik Abdullah <sheikabdullah.web@gmail.com>
2025-09-01 00:25:49 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://chat-app-backend-o74g.onrender.com/",
     withCredentials: true
 })
 
